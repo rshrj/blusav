da391f956f418d8c529ce2db84e9cabfa07678dd
Rishi Raj <rishiraj.1012exp@gmail.com>
2020-04-06 20:14:44 +0530

    search

--- _template/js/main.js
+++ _template/js/main.js
@@ -1,5 +1,25 @@
 window.onscroll = scrollFunction;
-window.onload = scrollFunction;
+window.onload = function () {
+  activateSearchBtn();
+  scrollFunction;
+};
+
+function activateSearchBtn() {
+  var searchBox = document.querySelector(".search-box");
+  var searchInput = document.querySelector(".search-box .search-input");
+  var searchButton = document.querySelector(".search-box .btn-search");
+  searchBox.addEventListener("mouseover", function (ep) {
+    searchInput.focus();
+  });
+  searchButton.addEventListener("click", function (e) {
+    alert("Not implementeed yet!");
+  });
+  searchInput.addEventListener("keyup", function (e) {
+    if (e.keyCode == 13) {
+      searchButton.click();
+    }
+  });
+}
 
 function scrollFunction() {
   var nav = document.querySelector("nav");
